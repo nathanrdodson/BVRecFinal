5cefb1a4c160485bfd35d76a5d0d719fa604c72d
Nathan Dodson <nathanrdodson@gmail.com>
2018-12-18 08:58:02 -0600

    Alpha Version

---
+++ BVRecFinal/FieldListButton.swift
@@ -0,0 +1,21 @@
+//
+//  FieldListButton.swift
+//  BVRecFinal
+//
+//  Created by Nathan R. Dodson on 12/10/18.
+//  Copyright © 2018 Nathan Dodson. All rights reserved.
+//
+
+import UIKit
+
+class FieldListButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
